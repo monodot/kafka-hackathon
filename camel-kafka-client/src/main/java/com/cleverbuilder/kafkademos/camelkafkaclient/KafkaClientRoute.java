2afdac1f5494870bf7e8a2e4135f32a778f72f76
Tom Donohue <tom@monodot.co.uk>
2017-12-08 11:57:06 +0000

    Stuff

--- camel-kafka-client/src/main/java/com/cleverbuilder/kafkademos/camelkafkaclient/KafkaClientRoute.java
+++ camel-kafka-client/src/main/java/com/cleverbuilder/kafkademos/camelkafkaclient/KafkaClientRoute.java
@@ -1,20 +1,23 @@
-package com.example;
- 
+package com.cleverbuilder.kafkademos.camelkafkaclient;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
- 
+import org.apache.camel.component.kafka.KafkaConstants;
+
 @Component
 public class KafkaClientRoute extends RouteBuilder {
- 
+
     @Override
     public void configure() throws Exception {
         from("timer:mytimer").process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        exchange.getIn().setBody("Test Message from Camel Kafka Component Final",String.class);
-                        exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY, 0);
-                        exchange.getIn().setHeader(KafkaConstants.KEY, "1");
-                    }
-                }).to("kafka:kafka:9092?topic=test");
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                exchange.getIn().setBody("Test Message from Camel Kafka Component Final", String.class);
+                exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY, 0);
+                exchange.getIn().setHeader(KafkaConstants.KEY, "1");
+            }
+        }).to("kafka:kafka:9092?topic=test");
     }
 }
